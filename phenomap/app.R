5825626d081fb6994d4d3e49a5e4589f0bbc9b85
Matthew Collins <mcollins@acis.ufl.edu>
2017-12-14 11:47:51 -0500

    Merge in code from heatmap and tree files. Trees are not quite working yet.

--- phenomap/app.R
+++ phenomap/app.R
@@ -1,5 +1,7 @@
 library(shiny)
 library(tidyr)
+library(gtable)
+library(grid)
 
 # Data set of Phenoscape characters + iDigBio specimens joined by taxon. See 
 # https://github.com/phenoscape/KB-DataFest-2017-linking-data/blob/master/Build_Specimen_List_by_Taxonomy.ipynb
@@ -10,9 +12,12 @@ pheno_specimens[pheno_specimens==''] <- NA
 
 # Re-shape to long form to make filtering on characters possible, remove NAs since the wide
 # matrix may have been sparse.
-pheno_specimens_long <- gather(pheno_specimens, character, value, 
+pheno_specimens_long <- gather(pheno_specimens, character, value,
                                char_sesamoid_bone_of_manus:char_pelvic_sucking_disc) %>%
-                        filter(!is.na(value))
+                        filter(!is.na(value)) %>%
+                        filter(value!='-60.0230556')
+pheno_specimens_long$value[pheno_specimens_long$value=='1 and 0'] <- '0 and 1'
+
 
 # Index of taxa-characters to use with UI controls
 taxa_chars_index <- pheno_specimens_long %>%
@@ -28,11 +33,45 @@ pheno_taxa_vector <- setNames(distinct_taxa[["vto_short"]], distinct_taxa[["vto_
 # requires elements to be labeled with vto_label for display
 active_taxa_vector <- c()
 # Hard-code xamples for the demo, also pick the first item in the list from full data set
-active_taxa_vector <- setNames(c("VTO:0062083","VTO:0043441"), c("Adontosternarchus sachsi", "Anabantidae"))
+#active_taxa_vector <- setNames(c("VTO:0062083","VTO:0043441"), c("Adontosternarchus sachsi", "Anabantidae"))
 
 # Map data and setup
 world <- map_data("world")
 map_height = 600
+
+# Heat map setup
+
+##############
+## import ott numbers
+ott<-read.csv('../data/phenoscape_taxonomy_ottids.csv')
+colnames(ott)[2]<-'vto_short'
+ott$vto_short<-str_replace(ott$vto_short, '_',':')
+ott$vto_short<-str_replace(ott$vto_short, ' ','')
+
+char_names<-colnames(pheno_specimens[grep("^char",colnames(pheno_specimens))])
+pheno_specimens %>%  group_by(vto_short,family,genus,specificepithet) %>% summarize(count=n()) %>% as.data.frame()-> pheno_specimens_sum
+pheno_specimens %>% select(vto_short,char_names)  %>% unique() %>% as.data.frame() -> pheno_specimens_charvals
+pheno_specimens_chars_merged<-merge(pheno_specimens_sum,pheno_specimens_charvals,by='vto_short') 
+pheno_specimens_chars_ott<-merge(pheno_specimens_chars_merged,ott,by='vto_short')
+pheno_specimens_chars_ott$genus_species<-paste(pheno_specimens_chars_ott$genus,pheno_specimens_chars_ott$specificepithet,sep=' ')
+hm<-melt(pheno_specimens_chars_ott,id.vars=c('vto_short','family','genus','genus_species','ott_id','count','vto_label'),
+         measure.vars = c(char_names)) %>% filter(value!='-60.0230556')
+hm$value[hm$value=='1 and 0'] <- '0 and 1'
+
+# Tree data setup
+char_names<-colnames(pheno_specimens[grep("^char",colnames(idig))])
+pheno_specimens %>%  group_by(vto_short,family,genus,specificepithet) %>% summarize(count=n()) %>% as.data.frame()-> idig_sum
+pheno_specimens %>% select(vto_short,char_names)  %>% unique() %>% as.data.frame() -> idig_charvals
+
+ott$vto_short <-  stringr::str_replace(ott$vto_short, ' ', '' )
+ott$vto_short <-  stringr::str_replace(ott$vto_short, '_', ':' )
+
+
+idig_chars_merged<-merge(idig_sum,idig_charvals,by='vto_short') 
+idig_chars_ott<-merge(idig_chars_merged,ott)
+idig_sum$genus_species<-paste(idig_sum$genus,idig_sum$specificepithet,sep=' ')
+
+
 
 
 ui <- fluidPage(
@@ -61,17 +100,19 @@ ui <- fluidPage(
            )
     ),
     column(9,
-           h2("Map"),
+           h2("Map of Selected Character for Active Taxa"),
            div(style = paste0("height:", map_height, "px"), plotOutput("map"))
            )
   ),
   
   fluidRow(
     column(6,
-           h2("Heatmap")
+           h2("Heatmap"),
+           plotOutput("heatmap")
            ),
     column(6,
-           h2("Phylogeny")
+           h2("Phylogeny"),
+           plotOutput("phylogeny")
            )
   )
 )
@@ -100,17 +141,17 @@ server <- function(input, output, session) {
     
     
     # Calculate available characters based on the active taxa
-#    # This method returns the union set of all characters of any active taxa
-#    chars_vector <- (taxa_chars_index %>%
-#                             filter(vto_short %in% active_taxa_vector) %>%
-#                             distinct(character))[["character"]]
-    # This method returns the intersection set of characters that all active taxa have
+    # This method returns the union set of all characters of any active taxa
     chars_vector <- (taxa_chars_index %>%
-                       filter(vto_short %in% active_taxa_vector) %>%
-                       group_by(character) %>%
-                       count() %>%
-                       filter(n == length(active_taxa_vector)) %>%
-                       distinct(character))[["character"]]
+                             filter(vto_short %in% active_taxa_vector) %>%
+                             distinct(character))[["character"]]
+    # This method returns the intersection set of characters that all active taxa have
+#    chars_vector <- (taxa_chars_index %>%
+#                       filter(vto_short %in% active_taxa_vector) %>%
+#                       group_by(character) %>%
+#                       count() %>%
+#                       filter(n == length(active_taxa_vector)) %>%
+#                       distinct(character))[["character"]]
     updateSelectInput(session, "selected_char", choices=chars_vector)
     
   })
@@ -126,8 +167,52 @@ server <- function(input, output, session) {
       scale_y_continuous(breaks=(-2:2) * 30) +
       scale_x_continuous(breaks=(-4:4) * 45) +
       theme_bw()
-    worldmap + geom_point(data=map_points, aes(x=lon, y=lat, color=vto_label, shape=value), size=5)
+    worldmap + geom_point(data=map_points, aes(x=lon, y=lat, color=value, shape=vto_label), size=5)
   }, height=map_height)
+  
+  
+  output$heatmap <- renderPlot({
+    print(input$selected_char)
+    
+    # Set aside the idea of pretty character names for now
+    #x_charlabs<-str_replace_all(str_to_title(str_replace(char_names,'char_','')),'_',' ')
+    
+    chars<-ggplot(hm %>% filter(vto_short %in% input$active_taxa), aes(variable,genus,fill=value)) + geom_tile() + 
+      scale_fill_discrete(na.value = 'white',labels=c('1'='present','0'='absent','0 and 1'='ambiguous'),name='Character State: ') +
+      labs(y=NULL,x='Phenoscape Character') + theme(axis.title.y=element_blank(),axis.text.y=element_blank(), legend.position="top",
+                                                    axis.text.x=element_text(angle=-90,hjust = 0)) #+ 
+     # scale_x_discrete(labels=c(x_charlabs))
+    
+    specimens<-ggplot(hm %>% filter(vto_short %in% input$active_taxa), aes(' ',genus,fill=count)) + geom_tile() + 
+      scale_fill_gradient(trans="log",low='white',high='blue',breaks=c(1,10000)) + 
+      theme(legend.position="top",legend.title = element_blank(),axis.text.x=element_text(angle=-90,hjust = 0),
+            axis.title.x = element_blank()) +
+      scale_x_discrete(labels='Number of Museum Specimens')
+    
+    plot_grid(specimens,chars,rel_widths = c(1, 3),align='h')  
+    
+  })
+  
+  output$phylogeny <- renderPlot({
+    print(input$selected_char)
+    idig_otol_subset <- idig_chars_merged[which(ott$vto_short %in% idig_chars_merged$vto_short),]
+    ott_l <- ott[which(ott$vto_short %in% idig_otol_subset$vto_short),]
+    ott_l <- ott_l[,4]
+    ol <- str_replace(ott_l, 'ott', '')
+    
+    
+    ol <- as.numeric(ol)
+    excl <- c(609231, 87893, 618300, 933436)
+    
+    for (item in excl){
+      ol <- ol[ol != item]
+    }
+    
+    
+    tr <- tol_induced_subtree(ott_ids=ol)
+    plot(tr)
+  })
+
 }
 
 
