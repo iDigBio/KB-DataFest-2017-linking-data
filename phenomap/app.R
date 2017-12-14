7102dff3c34b589ff62181f00032775e149b6218
Matthew Collins <mcollins@acis.ufl.edu>
2017-12-14 00:08:50 -0500

    Improve linked controls, layout, and sizes of map

--- phenomap/app.R
+++ phenomap/app.R
@@ -4,24 +4,31 @@ library(tidyr)
 # Pheno characters + iDigBio specimens joined by taxon
 pheno_specimens <- read.csv("../data/pheno_specimen_with_chars_tiny.csv", stringsAsFactors = FALSE)
 
-# Calculate the taxonomy from unique entries in the actual data file
-distinct_taxa <- pheno_specimens %>% distinct(vto_short, vto_label)
-pheno_taxa_vector <- setNames(distinct_taxa[["vto_short"]], distinct_taxa[["vto_label"]])
+# Re-shape to long form to make filtering on characters possible, remove NAs since the wide
+# matrix may have been sparse.
+pheno_specimens_long <- gather(pheno_specimens, character, value, 
+                               char_sesamoid_bone_of_manus:char_pelvic_sucking_disc) %>%
+                        filter(!is.na(value))
 
-# Re-shape to long form to make filtering on characters possible
-pheno_specimens_long <- gather(pheno_specimens, character, value, char_sesamoid_bone_of_manus:char_pelvic_sucking_disc)
+# Index of taxa-characters to use with UI controls
+taxa_chars_index <- pheno_specimens_long %>%
+                       distinct(vto_short, vto_label, character)
+
+# Calculate the taxonomy list from unique entries in the index
+distinct_taxa <- taxa_chars_index %>% distinct(vto_short, vto_label)
+pheno_taxa_vector <- setNames(distinct_taxa[["vto_short"]], distinct_taxa[["vto_label"]])
 
 # Global var of vto_shorts for tracking what should be listed in the checkboxgroup, 
 # requires elements to be labeled with vto_label for display
 active_taxa_vector <- c()
 
-# Global var of character column names
-active_chars_vector <- c()
+
 
 ##############
 ## plot data by coords
 world <- map_data("world")
 
+map_height = 600
 
 ui <- fluidPage(
   
@@ -29,7 +36,7 @@ ui <- fluidPage(
   titlePanel("Phenomap"),
   
   fluidRow(
-    column(6, 
+    column(3, 
            fluidRow(
              column(6,
                     h2("Select Taxa"),
@@ -43,22 +50,17 @@ ui <- fluidPage(
              )
            ),
            fluidRow(
-             column(6,
+             column(12,
                     h2("Select Character"),
-                    selectInput("selected_char", "", choices=active_chars_vector)
+                    selectInput("selected_char", "", choices=c())
                     # No longer allowin selection of multiple characters
                     #actionButton("add_character", "Add")
-             ),
-             column(6
-                    # No longer allowin selection of multiple characters
-                    #h2("Active Characters"),
-                    #checkboxGroupInput("active_chars", "", choices=active_chars_vector)
              )
            )
     ),
-    column(6,
+    column(9,
            h2("Map"),
-           plotOutput("map")
+           div(style = paste0("height:", map_height, "px"), plotOutput("map"))
            )
   ),
   
@@ -80,8 +82,7 @@ server <- function(input, output, session) {
     active_taxa_vector <<- c()
     print(active_taxa_vector)
     updateCheckboxGroupInput(session, "active_taxa", 
-                             choices=active_taxa_vector,
-                             selected=active_taxa_vector
+                             choices=character(0)
     )
   })
   
@@ -90,9 +91,7 @@ server <- function(input, output, session) {
       input$selected_taxon,
       (distinct_taxa %>% filter(vto_short == input$selected_taxon))[["vto_label"]]
     )
-    #print(new_choice)
     active_taxa_vector <<- c(active_taxa_vector, new_choice) # used to have unique() here but that function eats names of elements
-    #print(active_taxa_vector)
     updateCheckboxGroupInput(session, "active_taxa", 
                              choices=active_taxa_vector,
                              selected=active_taxa_vector
@@ -100,25 +99,21 @@ server <- function(input, output, session) {
     
     
     # Calculate available characters based on the active taxa
-    active_chars_vector <<- (pheno_specimens_long %>%
-                             filter(vto_short %in% active_taxa_vector) %>%
-                             distinct(character))
-    print(input$active_taxa)
-    print(active_chars_vector)
-    #[["character"]]
-    updateSelectInput(session, "selected_char", choices=active_chars_vector)
+#    # This method returns the union set of all characters of any active taxa
+#    chars_vector <- (taxa_chars_index %>%
+#                             filter(vto_short %in% active_taxa_vector) %>%
+#                             distinct(character))[["character"]]
+    # This method returns the intersection set of characters that all active taxa have
+    chars_vector <- (taxa_chars_index %>%
+                       filter(vto_short %in% active_taxa_vector) %>%
+                       group_by(character) %>%
+                       count() %>%
+                       filter(n == length(active_taxa_vector)) %>%
+                       distinct(character))[["character"]]
+    updateSelectInput(session, "selected_char", choices=chars_vector)
     
   })
 
-# No longer allowing multiple characters  
-#  observeEvent(input$add_character, {
-#    active_chars_vector <<- c(active_chars_vector, input$selected_char)
-#    updateCheckboxGroupInput(session, "active_chars", 
-#                             choices=active_chars_vector,
-#                             selected=active_chars_vector
-#    )
-#  })
-  
   output$map <- renderPlot({
     map_points <- pheno_specimens_long %>%
                     filter(vto_short %in% input$active_taxa) %>%
@@ -130,8 +125,8 @@ server <- function(input, output, session) {
       scale_y_continuous(breaks=(-2:2) * 30) +
       scale_x_continuous(breaks=(-4:4) * 45) +
       theme_bw()
-    worldmap + geom_point(data=map_points, aes(x=lon, y=lat, color=vto_label, shape=value))
-  })
+    worldmap + geom_point(data=map_points, aes(x=lon, y=lat, color=vto_label, shape=value), size=5)
+  }, height=map_height)
 }
 
 
