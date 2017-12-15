d79e004c9033b61c217a1f51411608279cbb3903
blubbundbla <martha.kandziora@yahoo.com>
2017-12-15 10:39:51 -0800

    mapping trait absence

--- climate_things.R
+++ climate_things.R
@@ -95,41 +95,64 @@ head(idig)
 idig$"spn" <- paste(idig$genus, idig$specificepithet, sep="_")
 cleaned.df <- idig[c(25,7, 9:24)]
 
-
 world <- map_data("world")
 
+as.numeric(as.character(cleaned.df[5:18]))
+
+mode(cleaned.df[5:18]) <- "numeric"
+
+
+library(dplyr)
 
 ### map occurences of traits present
 for (char in colnames(cleaned.df[c(5:18)]))
 {
-  
+  #char <- colnames(cleaned.df[8])
   print(char)
   col <- which(colnames(cleaned.df)==char)
   sub_char <- na.omit(cleaned.df[c(1:4,col)], cols= cleaned.df[c(col, 3:4)])  
     
   char_present <- subset(sub_char, sub_char[char]=="1")
+  char_absent <- subset(sub_char, sub_char[char]=="0")
+  char_comb <-subset(sub_char, sub_char[char]=="0" || sub_char[char]=="1")
+
+  
+  library(RColorBrewer)
+  myColors <- brewer.pal(5,"Set1")
+  names(myColors) <- levels(char_comb[char])
+  colScale <- scale_colour_manual(name = "char",values = myColors)
+  
+  char_comb$fchar <- factor(char_comb$char_humerus)
     
   worldmap <- ggplot() +  geom_path(data=world, aes(x=long, y=lat, group=group))+  scale_y_continuous(breaks=(-2:2) * 30) +
       scale_x_continuous(breaks=(-4:4) * 45) + theme_bw()
     
-  worldmap + geom_point(data=char_present, aes(x=lon,y=lat,color=char))
-  
+  worldmap + geom_point(data=char_comb, aes(x=lon,y=lat, colour=fchar))
+ 
   #### plotting occurences of taxa with trait presence
   ### !this represents partly only that sampling in some regions is denser than in others!
   
   par(cex.main=0.9, mfrow=c(1,2), mar=c(4,4,1,1), mgp=c(2,0.5,0), tcl=0.2)
-  hist(char_present$lon, breaks=100, main="Longitidunal distribution of species with trait present", xlab="Longitude")
-  hist(char_present$lat, breaks=100, main="Latitudinal distribution of species with trait present", xlab="Latitude")
-  
-  
-  
-  input <- na.omit(char_present[c(1,3,4)])
-  colnames(input) <- c("speciesname", "latitude", "longitude")
-  clim_occ <- get_worldclim_prectemp(input, return="all")
-  par(cex.main=0.9, mfrow=c(1,2), mar=c(4,4,1,1), mgp=c(2,0.5,0), tcl=0.2)
-  with(clim_occ, hist(MAT, main="Mean annual temperature of species with the trait present based on all occurence points for the species in IDigBio"))
-  with(clim_occ, hist(MAP, main="Mean annual precipitation of species with the trait present based on all occurence points for the species in IDigBio"))
+  hist(char_present$lon, breaks=100, main="Longitidunal distribution of species with trait present", xlab="Longitude", col="lightgreen")
+  hist(char_absent$lon, breaks=100, color="red", add=T)
+  hist(char_present$lat, breaks=100, main="Latitudinal distribution of species with trait present", xlab="Latitude", col="lightgreen")
+  hist(char_absent$lat, breaks=100, add=T, col="red")
 
+
+
+  input1 <- na.omit(char_present[c(1,3,4)])
+  colnames(input1) <- c("speciesname", "latitude", "longitude")
+  clim_occ1 <- get_worldclim_prectemp(input1, return="all")
+  
+  input2 <- na.omit(char_absent[c(1,3,4)])
+  colnames(input2) <- c("speciesname", "latitude", "longitude")
+  clim_occ2 <- get_worldclim_prectemp(input2, return="all")
+  
+  par(cex.main=0.9, mfrow=c(1,2), mar=c(4,4,1,1), mgp=c(2,0.5,0), tcl=0.2)
+  with(clim_occ1, hist(MAT, main="Mean annual temperature of species with the trait present based on all occurence points for the species in IDigBio", col="lightgreen"))
+  with(clim_occ2, hist(MAT, add=T, col="red"))
+  with(clim_occ1, hist(MAP, main="Mean annual precipitation of species with the trait present based on all occurence points for the species in IDigBio", col="lightgreen"))
+  with(clim_occ2, hist(MAP, add=T, col="red"))
   
 }  
 
